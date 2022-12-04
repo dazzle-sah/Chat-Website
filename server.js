59316b077f3ec5564e34b96b85b1da589b64047b
dazzle-sah <drishti.sah1412@gmail.com>
2022-12-04 14:37:58 +0530

    join

--- server.js
+++ server.js
@@ -13,3 +13,28 @@ const {
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = 'Chat Bot';
+
+
+// Run when client connects
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    const roomName = room;
+
+    socket.join(user.room);
+
+    // Welcome current user
+    socket.emit('message', formatMessage(botName, `Welcome to ${roomName} Room`));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
