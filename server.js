0404bc9b290589fdac53535eeaa1474157f499a5
dazzle-sah <drishti.sah1412@gmail.com>
2022-12-04 14:38:44 +0530

    final-server

--- server.js
+++ server.js
@@ -38,3 +38,40 @@ io.on('connection', socket => {
         'message',
         formatMessage(botName, `${user.username} has joined the chat`)
       );
+      // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    });
+  
+    // Listen for chatMessage
+    socket.on('chatMessage', msg => {
+      const user = getCurrentUser(socket.id);
+  
+      io.to(user.room).emit('message', formatMessage(user.username, msg));
+    });
+  
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+      const user = userLeave(socket.id);
+  
+      if (user) {
+        io.to(user.room).emit(
+          'message',
+          formatMessage(botName, `${user.username} has left the chat`)
+        );
+  
+        // Send users and room info
+        io.to(user.room).emit('roomUsers', {
+          room: user.room,
+          users: getRoomUsers(user.room)
+        });
+      }
+    });
+  });
+  
+  const PORT = process.env.PORT || 3000;
+  
+  server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  
